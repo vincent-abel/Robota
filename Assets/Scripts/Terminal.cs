38cf2104c831c7fed6bab64610846d0ee97420dd
Vincent ABEL-GRIMALT <vincent.abel.grimalt@gmail.com>
2021-05-04 16:41:36 +0200

    Added component to Panel for the hierarchy
    Signed-off-by: Vincent ABEL-GRIMALT <vincent.abel.grimalt@gmail.com>

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -16,32 +16,46 @@ public class Terminal
     {
         GameObject mainCanvasGO = new GameObject();
         GameObject Panel = new GameObject();
+        GameObject consoleGO = new GameObject();
+        GameObject consoleInputGO = new GameObject();
+
         Panel.name = "Terminal2";      
         mainCanvasGO.name = "mainCanvas";
         Panel.transform.parent = mainCanvasGO.transform;
         mainCanvasGO.AddComponent<Canvas>();
         mainCanvasGO.AddComponent<CanvasScaler>();
         mainCanvasGO.AddComponent<GraphicRaycaster>();
-        mainCanvasGO.AddComponent>();
+        Panel.AddComponent<CanvasRenderer>();
         Panel.AddComponent<RectTransform>();
 
         Canvas mainCanvas = mainCanvasGO.GetComponent<Canvas>();
         mainCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
         
 
-        console = Panel.AddComponent<Text>();
+        console = consoleGO.AddComponent<Text>();
         console.name ="console";
-        consoleInput = Panel.AddComponent<InputField>();
+        console.transform.parent = Panel.transform;
+        consoleGO.AddComponent<CanvasRenderer>();
+        consoleInput = consoleInputGO.AddComponent<InputField>();
         consoleInput.name = "Input";
+        consoleInputGO.AddComponent<CanvasRenderer>();
+        
+        consoleInput.transform.parent = Panel.transform;
 
         RectTransform consoleRT = console.GetComponent<RectTransform>();
         RectTransform cInputRT = consoleInput.GetComponent<RectTransform>();
         RectTransform canvasRT = mainCanvasGO.GetComponent<RectTransform>();
+        RectTransform PanelRT = Panel.GetComponent<RectTransform>();
 
+        
         consoleRT.anchoredPosition3D = new Vector3(0f,0f,0f);
         consoleRT.sizeDelta = new Vector2(0f,0f);
         consoleRT.anchorMin = new Vector2(0f,0f);
         consoleRT.anchorMax = new Vector2(1f,1f);
+        PanelRT.anchoredPosition3D = new Vector3(0f,0f,0f);
+        PanelRT.sizeDelta = new Vector2(0f,0f);
+        PanelRT.anchorMin = new Vector2(0f,0f);
+        PanelRT.anchorMax = new Vector2(1f,1f);
         console.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
 
         console.text = "HELLOW WORLD";
